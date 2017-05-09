5732147dc3fd52d1093769cd11f2c04532eadc57
Eimantas Zlabys <eimantas.zlabys@devbridge.com>
2017-05-09 12:49:52 +0300

    rewrite login logic

--- src/app/containers/Login.jsx
+++ src/app/containers/Login.jsx
@@ -1,5 +1,7 @@
 import React, { PropTypes, Component } from 'react';
+import { Redirect } from 'react-router-dom'; 
 import axios from 'axios';
+
 import $ from 'jquery';
 
 import { Cookies } from 'react-cookie';
@@ -9,51 +11,67 @@ class Login extends Component{
 
     constructor(props){
         super(props);
+
         this.state = {
             loginEmail: '',
             loginPassword: '',
             registerName: '',
             registerPassword: '',
-            registerEmail: ''
+            registerEmail: '',
+            errorMessage: ''
         };
+
+        this.handleSignUp = this.handleSignUp.bind(this);
+        this.handleSignIn = this.handleSignIn.bind(this);
 
         this.handleChange = this.handleChange.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
-        this.handleCreate = this.handleCreate.bind(this);
     }
     
     handleChange(event) {
+        $('#loginalert').hide();
+
         this.setState({
             [event.target.name]: event.target.value
         });
     }
+
+    validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
     
     handleLogin() {
-        axios.post('http://localhost:5001/api/accounts/login',
-        {
-            "Email": this.state.loginEmail,
-            "Password": this.state.loginPassword
-        },
-        {
+        var me = this;
+
+        if(this.validateEmail(this.state.loginEmail)){
+            axios.post('http://localhost:5001/api/accounts/login',
+            {
+                "Email": this.state.loginEmail,
+                "Password": this.state.loginPassword
+            },
+            {
             headers: {
                 "Content-Type": "application/json"
             }
-        })
-        .then(function (response) {
-            console.log('login',response);
-            cookies.set('edoratorAccessToken', response.data.accessToken, {
-                expires: new Date(Date.now + response.data.expiresIn)
-            });
+            }).then(function(response){
+                $('#loginalert').hide();
 
-            <Redirect to="/"/>
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-    }
-    
-    handleCreateAccount() {
-        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+                cookies.set('edoratorAuthTicket', response.data.accessToken);
+
+                <Redirect to={'/home'} />
+            }).catch(function(error){
+                me.setState({
+                    errorMessage: error.toString()
+                })
+                $('#loginalert').show();
+            });
+        }else{
+            this.setState({
+                errorMessage: "Error: Bad email or password"
+            })
+            $('#loginalert').show();
+        }
     }
 
     handleCreate() {
@@ -75,24 +93,120 @@ class Login extends Component{
             throw Error(error);
         });
     }
+    
+    handleSignUp() {
+        $('#loginbox').hide(); 
+        $('#signupbox').show();
+    }
+
+    handleSignIn() {
+        $('#signupbox').hide();
+        $('#loginbox').show();
+    }
 
     render(){
         return (
-            <div className="login-page">
-                <div className="form">
-                    <form className="register-form">
-                        <input name="registerName" type="text" onChange={this.handleChange} placeholder="name"/>
-                        <input name="registerPassword" type="password" onChange={this.handleChange} placeholder="password"/>
-                        <input name="registerEmail" type="email" onChange={this.handleChange} placeholder="email address"/>
-                        <button onClick={this.handleCreate}>create</button>
-                        <p className="message">Already registered? <a href="#" onClick={this.handleCreateAccount}>Sign In</a></p>
-                    </form>
-                    <form className="login-form">
-                        <input name="loginEmail" value={this.props.loginEmail} type="email" placeholder="Email"/>
-                        <input name="loginPassword" value={this.props.loginPassword} type="password" placeholder="Password"/>
-                        <button onClick={this.handleLogin}>login</button>
-                        <p className="message">Not registered? <a href="#" onClick={this.handleCreateAccount}>Create an account</a></p>
-                    </form>
+            <div className="container">
+                 <div id="loginbox" style={{marginTop: '50px'}} className="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2"> 
+                     <div className="panel panel-info">
+                        <div className="panel-heading">
+                            <div className="panel-title">Sign In</div>
+                            <div style={{float:'right', fontSize: '80%', position: 'relative', top:'-10px'}}><a href="#">Forgot password?</a></div>
+                        </div>   
+
+                        <div style={{paddingTop:'30px'}} className="panel-body">
+                            <div style={{display:'none'}} id="login-alert" className="alert alert-danger col-sm-12"></div>
+                            
+                            <form id="loginform" className="form-horizontal" role="form">
+                                <div id="loginalert" style={{display: 'none'}} className="alert alert-danger">
+                                    <p>{this.state.errorMessage}</p>
+                                    <span></span>
+                                </div>
+
+                                <div style={{marginBottom: '25px'}} className="input-group">
+                                    <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                                    <input id="login-username" type="email" className="form-control" onChange={this.handleChange} name="loginEmail" value={this.state.loginEmail} placeholder="email"/>                                        
+                                </div>
+                                
+                                 <div style={{marginBottom: '25px'}} className="input-group">
+                                    <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
+                                    <input id="login-password" type="password" className="form-control" onChange={this.handleChange} value={this.state.loginPassword}  name="loginPassword" placeholder="password" />
+                                </div>
+
+                                 <div className="input-group">
+                                    <div className="checkbox">
+                                        <label>
+                                            <input id="login-remember" type="checkbox" name="remember" value="1" /> Remember me
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <div style={{marginTop: '10px'}} className="form-group">
+                                    <div className="col-sm-12 controls">
+                                      <a id="btn-login" onClick={this.handleLogin} className="btn btn-success">Login  </a>
+                                    </div>
+                                </div>
+
+                                <div className="form-group">
+                                    <div className="col-md-12 control">
+                                        <div style={{borderTop: '1px solid#888', paddingTop: '15px', fontSize: '85%'}} >
+                                            Don't have an account! 
+                                        <a href="#" onClick={this.handleSignUp}>
+                                            Sign Up Here
+                                        </a>
+                                        </div>
+                                    </div>
+                                </div>    
+                            </form>     
+                        </div>
+                    </div>
+                </div>
+                <div id="signupbox" style={{display: 'none', marginTop: '50px'}} className="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                     <div className="panel panel-info">
+                         <div className="panel-heading">
+                            <div className="panel-title">Sign Up</div>
+                            <div style={{float: 'right', fontSize: '85%', position: 'relative', top: '-10px'}}><a id="signinlink" href="#" onClick={this.handleSignIn}>Sign In</a></div>
+                        </div> 
+                        <div className="panel-body" >
+                            <form id="signupform" className="form-horizontal" role="form">
+                                <div id="signupalert" style={{display: 'none'}} className="alert alert-danger">
+                                    <p>Error:</p>
+                                    <span></span>
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="email" className="col-md-3 control-label">Email</label>
+                                    <div className="col-md-9">
+                                        <input type="text" className="form-control" name="email" placeholder="Email Address" />
+                                    </div>
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="firstname" className="col-md-3 control-label">First Name</label>
+                                    <div className="col-md-9">
+                                        <input type="text" className="form-control" name="firstname" placeholder="First Name" />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="lastname" className="col-md-3 control-label">Last Name</label>
+                                    <div className="col-md-9">
+                                        <input type="text" className="form-control" name="lastname" placeholder="Last Name"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password" className="col-md-3 control-label">Password</label>
+                                    <div className="col-md-9">
+                                        <input type="password" className="form-control" name="passwd" placeholder="Password" />
+                                    </div>
+                                </div>
+                                <div className="form-group">                                     
+                                    <div className="col-md-offset-3 col-md-9">
+                                        <button id="btn-signup" type="button" className="btn btn-info"><i className="icon-hand-right"></i>Sign Up</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                     </div>
                 </div>
             </div>
         );
